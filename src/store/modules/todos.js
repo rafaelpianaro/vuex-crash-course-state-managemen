5924e0d9abc448bd9f494eef9cd55fb7624eb6b6
Rafael <rafaelpianaro@gmail.com>
2022-01-19 22:16:13 -0300

    *AddTodo.vue *computed *fetch using axios

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,29 +1,30 @@
 import axios from "axios";
 
 const state = {
-    todos: [
-        {
-            id: 1,
-            title: 'Todo one'
-        },
-        {
-            id: 2,
-            title: 'Todo two'
-        },
-        {
-            id: 3,
-            title: 'Todo three'
-        },
-    ]
+    todos: []
 }
 
 const getters = {
     allTodos: (state) => state.todos
 }
 
-const actions = {}
+const actions = {
+    async fetchTodos({ commit }){
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+        commit('setTodos', response.data)
+        console.log('response', response.data)
+    },
+    async addTodo({ commit }, title) {
+        const response = await axios.post('https://jsonplaceholder.typicode.com/todos', { title, completed: false })
+        // call a mutation
+        commit('newTodo', response.data)
+    }
+}
 
-const mutations = {}
+const mutations = {
+    setTodos: (state, todos) => (state.todos = todos),
+    newTodo: (state, todo) => state.todos.unshift(todo) // time 32:10
+}
 
 export default {
     state,
